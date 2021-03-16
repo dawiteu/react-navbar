26919292df89d57dd3c8aa09c6d22f3ccc20b61f
dawiteu <dawid@tararuj.be>
2021-03-16 09:55:21 +0100

    ffiiiin

--- src/component/About.js
+++ src/component/About.js
@@ -5,9 +5,11 @@ const About = () => {
 
     return (
         <Fragment>
-            <p>About nous: </p>
+            <p>About nous:     </p>   
+
+            
             <p>Nous sommes Lorem ipsum dolor sit.</p>
-        </Fragment>
+        </Fragment>   
     );
 }
 
