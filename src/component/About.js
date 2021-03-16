a293170f50761b12ec5763edf6b6828d3ee6b055
dawiteu <dawid@tararuj.be>
2021-03-16 10:11:06 +0100

    test

--- src/component/About.js
+++ src/component/About.js
@@ -6,8 +6,6 @@ const About = () => {
     return (
         <Fragment>
             <p>About nous:     </p>   
-
-            
             <p>Nous sommes Lorem ipsum dolor sit.</p>
         </Fragment>   
     );
